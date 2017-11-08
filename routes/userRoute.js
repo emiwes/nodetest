4a1174ac6cf40cd5965eef289c35613b118458ce
Emil Westin <emil.westin1@gmail.com>
2017-11-08 15:12:36 +0100

    mongoDM with mLAB hosting

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,25 +1,71 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-var userService = require('../services/userService');
+const userService = require('../services/userService');
+
+const User = mongoose.model('User', { 
+    name: String 
+});
+
 
 router.route('/user')    
     .get(function(req, res){
         res.send('getting user');
     })
+
     .post(function(req, res){
+        
+        let user = new User({ 
+            "name": req.body.name 
+        });
+
+        user.save(function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('meow');
+          }
+        });
+
         res.send('posting user');
     })
+
     .put(function(req, res){
-        res.send('putting user');
+        res.send('updating user');
     })
+
     .delete(function(req, res){
         res.send('deleting user');
     });
 
     router.route('/user/:id')
-    .get(function(req, res){
-        res.json({'result' : userService.multiplyUserId(req.params.id, 2)});
-    });
+        .get(function(req, res){
+            // res.json({'result' : userService.multiplyUserId(req.params.id, 2)});
+
+            User.findById(req.params.id, function(err, user){
+                if (err) {
+                    console.log(err);
+                }
+
+                res.json(user);
+            });
+        })
+
+        .put(function(req, res){
+
+            let newName = {
+                "name": req.body.name
+            }
+
+            User.findByIdAndUpdate(req.params.id, newName, function(err, user){
+                if (err) {
+                    console.log(err);
+                }
+
+                res.json(user);
+
+            });
+        });
 
  module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
-//var cors = require('cors');
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser')
+
+
 var userRoute = require('./routes/userRoute');
 var workRoute = require('./routes/workRoute');
+
+const mongoConnection = 'mongodb://fancypantz:viking1337@ds151355.mlab.com:51355/fancypantz';
+mongoose.connect(mongoConnection, { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
 var port = 3000 || proccess.env.PORT;
 var host = 'localhost';
@@ -11,8 +19,15 @@ var myLogger = function (req, res, next) {
     let d = new Date();
     console.log(`${d.getHours()}:${d.getMinutes()} ${req.method} request to ${req.path}`)
     next()
-} 
+}
 
+/* 
+    MIDDLEWARES 
+*/
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 app.use(myLogger);
 
 app.get('/', function(req, res){
